0e7ede9afd021120351ade07fbeca73b038cb4bf
Chad Hart <chadwallacehart>
2016-12-10 23:23:26 -0500

    status

--- scratchx_pirover.js
+++ scratchx_pirover.js
@@ -2,13 +2,29 @@
  * Created by chad on 12/9/16.
  */
 (function(ext) {
+
+    var xhr = new XMLHttpRequest();
+
+
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
+        xhr.open("GET", "https://192.168.100.31/status", false);
+        xhr.send();
+
+        if (xhr.status == "200"){
+            if (xhr.statusText == "ready")
+                return {status: 2, msg: 'Ready'};
+            else
+                return {status: 1, msg: 'Waiting'};
+        }
+        else {
+            return {status: 0, msg: 'Error'};
+        }
+
     };
 
     // Functions for block with type 'w' will get a callback function as the
@@ -42,3 +58,10 @@
     // Register the extension
     ScratchExtensions.register('Random wait extension', descriptor, ext);
 })({});
+
+//Failed attempt to write in front of the flash screen
+/*
+$(document).load(function(){
+    $("body").append('<div class="modal-content" stype="height:50%;width:50%;z-index:100"><p>Hello modal!</p></div>');
+});
+    */
